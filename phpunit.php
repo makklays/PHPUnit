586f1834eaa845d99712d8f22329df7505f2f05c
Kuziv Alexander <makklays@gmail.com>
2017-05-03 12:33:01 +0300

    Create phpunit.php

---
+++ phpunit.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * (c) Kuziv A.V. <makklays@gmail.com>
+ **/
+
+require_once '../vendor/autoload.php';
+use \PHPUnit\Framework\TestCase;
+
+class Sum
+{
+	function aplusb($a, $b){
+		return ( $a + $b );
+	}
+}
+
+$sosi = new Sum();
+$a = $sosi->aplusb(1,1);
+//echo $a;
+
+/**
+ * Пример модульного тестрования
+ **/
+class SumTest extends PHPUnit_Framework_TestCase
+{
+	private $obj;
+	// это метод, который вызывается перед каждым тестом
+	protected function setUp(){
+		$this->obj = new Sum();
+	}
+	// это метод, который вызывается после каждого теста
+	protected function tearDown(){
+		$this->obj = NULL;
+	}
+	// метод для тестрования метода aplusb()
+	public function testAplusb(){
+		$result = $this->obj->aplusb(1,1);	
+		$this->assertEquals(2,$result);
+	}
+}
+
+?>
